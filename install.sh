3005a55261d118bd625a1c355b3e48123a8ba82f
Mikhail Marasanov <mihamar2005@gmail.com>
2026-04-04 13:07:10 +0200

    init

---
+++ install.sh
@@ -0,0 +1,15 @@
+#bin/bash
+
+DOTFILES_DIR = "$(cd "$(dirname {BASH_SOURCE[0]}")" && pwd)"
+
+echo "Installing dotfiles..."
+
+mkdir -p ~/.config/niri
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/noctalia
+
+cp -r "$DOTFILES_DIR/niri/.config/niri" ~/.config/niri
+cp -r "$DOTFILES_DIR/alacritty/" ~/.config/alacritty
+cp -r "$DOTFILES_DIR/noctalia" ~/.config/noctalia
+
+echo "Done!"
